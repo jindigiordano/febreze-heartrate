3b04477c32d8f3b815338d3fce073769f6bbe941
Jin Di Giordano <jin.di.giordano@gmail.com>
2017-03-04 22:07:26 -0800

    Successfully calls API to change color of device

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,24 @@
 module ApplicationHelper
 
-  require 'net/http'
-
   def febreze_light
-    http = Net::HTTP.new("na-hackathon-api.arrayent.io:443")
+    require 'uri'
+    require 'net/http'
 
-    request = Net::HTTP::Put.new("/v3/devices")
-    request.body = [{"DeviceAction": "led_mode=1" }, {"DeviceAction": "led_color=0,1,4,4,4" }].to_json
-    request['Authorization'] = "Bearer #{ENV['FEBREZE']}"
-    request['Content-Type'] = 'application/json'
+    url = URI("https://na-hackathon-api.arrayent.io:443/v3/devices/33554441")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Put.new(url)
+    request["authorization"] = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiI2MDU5ZTExMC0wMTFhLTExZTctOTIwNy1iNWMzYjY2M2Y2YTQiLCJlbnZpcm9ubWVudF9pZCI6Ijk0OGUyY2YwLWZkNTItMTFlNi1hZTQ2LTVmYzI0MDQyYTg1MyIsInVzZXJfaWQiOiI5MDAwMDg0Iiwic2NvcGVzIjoie30iLCJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbl9jb2RlIiwiaWF0IjoxNDg4NjYzMzM0LCJleHAiOjE0ODk4NzI5MzR9.nJGTrEkQzNdx4f8FyMXkH3I7g7vh1AQ-cldkOv6HC_0JFjl3MC74vFgzk-wuSYw2r1MuYwI_uEbnKiDDTb65Fw'
+    request["content-type"] = 'application/json'
+    request["cache-control"] = 'no-cache'
+    request["postman-token"] = 'a94141b7-bbe3-6e72-5602-591644b18d92'
+    request.body = "[{\"DeviceAction\": \"led_mode=1\" }, {\"DeviceAction\": \"led_color=0,2,4,4,4\" }]"
+
     response = http.request(request)
+    puts response.read_body
   end
 
 end
